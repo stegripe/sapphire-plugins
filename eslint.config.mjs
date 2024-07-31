abd323a4267bbb298527518d5c54802c679a4802
noxzym <orchitiadi@gmail.com>
2024-07-31 13:46:21 +0700

    chore(eslint): update eslint config
    Co-authored-by: Fabio Mahardika Putra Juan <contact@fab1o.me>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
-import { common, ignores, modules, node, prettier, typescript } from "@stegripe/eslint-config";
+import { common, modules, node, prettier, typescript } from "@stegripe/eslint-config";
 import { Linter } from "eslint";
 
 /**
- * @type {Linter.FlatConfig[]}
+ * @type {Linter.Config[]}
  */
 const config = [
     ...common,
@@ -10,15 +10,15 @@ const config = [
     ...node,
     ...prettier,
     ...typescript,
-    ...ignores,
+    { ignores: ["**/node_modules", "**/dist"] },
     {
         languageOptions: {
             parserOptions: {
-                project: ["./tsconfig.eslint.json", "./packages/*/tsconfig.json"],
+                allowAutomaticSingleRunInference: true,
+                project: ["tsconfig.eslint.json", "packages/*/tsconfig.eslint.json"],
                 tsconfigRootDir: import.meta.dirname
             }
         },
-        ignores: ["./packages/*/dist/**"],
         rules: {
             "@typescript-eslint/no-base-to-string": "off",
             "@typescript-eslint/no-unnecessary-type-assertion": "off",
